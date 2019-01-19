cecc56e231436d775a134939f8232fb4f124b523
Koji Tanaka <koji.tanaka@binc.jp>
2019-01-19 19:27:38 +0900

    add Gear.gear_inches

--- chapter2/gear_test.rb
+++ chapter2/gear_test.rb
@@ -3,7 +3,12 @@ require_relative './gear'
 
 class GearTest < Minitest::Test
   def test_ratio
-    assert_equal(4.7272727272727272, Gear.new(52, 11).ratio)
-    assert_equal(1.1111111111111111, Gear.new(30, 27).ratio)
+    assert_equal(4.7272727272727272, Gear.new(52, 11, 25, 1.5).ratio)
+    assert_equal(1.1111111111111111, Gear.new(30, 27, 25, 1.5).ratio)
+  end
+
+  def test_inches
+    assert_equal(137.09090909090909, Gear.new(52, 11, 26, 1.5).gear_inches)
+    assert_equal(125.27272727272728, Gear.new(52, 11, 24, 1.25).gear_inches)
   end
 end
