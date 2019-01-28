8bc21187bf52cee6fe0fd820aef32713d49e7557
Koji Tanaka <koji.tanaka@binc.jp>
2019-01-29 00:29:28 +0900

    データ構造の隠蔽

---
+++ chapter2/obscuring_references.rb
@@ -0,0 +1,14 @@
+class ObscuringReferences
+  attr_reader :data
+  def initialize(data)
+    @data = data
+  end
+
+  def diameters
+    data.collect {|cell|
+      cell[0] + (cell[1] * 2)}
+  end
+end
+
+ref = ObscuringReferences.new([[622, 20], [622, 23], [559, 30], [559, 40]])
+puts ref.diameters
